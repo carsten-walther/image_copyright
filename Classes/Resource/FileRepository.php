fdfc3a2d06b6349a5cdcca1346faf110ef3f40b2
Carsten Walther <walther.carsten@web.de>
2026-04-13 09:06:25 +0200

    fix: filter images without copyright information on plugin option

--- Classes/Resource/FileRepository.php
+++ Classes/Resource/FileRepository.php
@@ -255,7 +255,7 @@ readonly class FileRepository extends \TYPO3\CMS\Core\Resource\FileRepository
                     $fileReferenceObject = $this->factory->getFileReferenceObject($referenceUid);
                     $fileExtension = $fileReferenceObject->getExtension();
                     if ($fileReferenceObject->isMissing() === false && in_array($fileExtension, $extensions, true) && file_exists(Environment::getPublicPath() . $fileReferenceObject->getPublicUrl()) === true) {
-                        if ($showEmpty === true || ($showEmpty === false && !empty($fileReferenceObject->hasProperty('copyright')))) {
+                        if ($showEmpty === true || ($showEmpty === false && $fileReferenceObject->hasProperty('copyright') && !empty($fileReferenceObject->getProperty('copyright')))) {
                             $pages = null;
                             if ($showUsedOnPage) {
                                 $pages = $this->generateFileReferencePages($fileReferenceObject, $tableFieldConfiguration);
